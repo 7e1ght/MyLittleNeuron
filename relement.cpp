ef9e80aff3b48c976d7e1283c4ce028f9edcb4a6
Vlad <7e1ght.jm@gmail.com>
2019-10-23 17:21:42 +0300

    add relement class

--- relement.cpp
+++ relement.cpp
@@ -1,6 +1,64 @@
 #include "relement.h"
 
-RElement::RElement()
+using namespace std;
+
+void RElement::writeCoefficients()
+{
+    coefficientsFile.open(::fileName, ios_base::out);
+
+    for (int i =0; i < ::signalsNum; i++)
+        coefficientsFile << coefficients[i] << " ";
+
+    coefficientsFile.close();
+}
+
+unsigned char RElement::getResult()
 {
 
+}
+
+void RElement::incorrentNotNine()
+{
+    for(int i = 0; i < ::signalsNum; i++)
+        if(input[i] == 1)
+        {
+            coefficients[i] -= 1;
+            writeCoefficients();
+        }
+
+    writeCoefficients();
+}
+
+void RElement::incorrectNine()
+{
+    for(int i = 0; i < ::signalsNum; i++)
+        if(input[i] == 1)
+        {
+            coefficients[i] += 1;
+            writeCoefficients();
+        }
+
+    writeCoefficients();
+}
+
+void RElement::setInput(unsigned char* signals)
+{
+    for(int i = 0; i < ::signalsNum; i++)
+        input[i] = signals[i];
+}
+
+RElement::RElement()
+{
+    coefficientsFile.open(fileName, ios_base::in);
+
+    if(coefficientsFile.is_open())
+    {
+        for (int i = 0; i < ::signalsNum; i++) {
+            int c;
+            coefficientsFile >> c;
+            coefficients[i] = c;
+        }
+    }
+
+    coefficientsFile.close();
 }
--- relement.h
+++ relement.h
@@ -1,10 +1,30 @@
 #ifndef RELEMENT_H
 #define RELEMENT_H
 
+#include <fstream>
+
+namespace
+{
+    const int signalsNum = 15;  // Количество S-елементов
+    const std::string fileName = "coef.txt"; // Файл с коэффициентами
+}
 
 class RElement
 {
+private:
+    std::fstream coefficientsFile;
+    int coefficients[::signalsNum];
+    unsigned char input[::signalsNum]; // Входные сигналы
+
+    void writeCoefficients();
 public:
+    void setInput(unsigned char* signals);
+    unsigned char getResult();
+
+    // Обучение
+    void incorrectNine();       // Ввод девять; вывод, что не девять
+    void incorrentNotNine();    // Ввод не девяти; вывод, что девять
+
     RElement();
 };
 
